f0e187538fe7d7c925f8247632b0f5b59a8b0912
K3rllon <francisco.silva9654@aluno.ce.gov.br>
2024-08-21 22:44:27 -0300

    Terceiro commit

---
+++ atividades_laco_de_repeticao/q03_lc.py
@@ -0,0 +1,12 @@
+import time
+
+print ('...💣.Bomba.💣...')
+
+n = 5
+
+while n >= 0:
+    print(f'... {n}')
+    time.sleep(1)  
+    n -= 1
+
+print("Boom 💥💀 !!")
---
+++ atividades_laco_de_repeticao/q04_lc.py
@@ -0,0 +1,5 @@
+for i in range (1,11,1):
+ for j in range (1,11,1):
+    print(f"{i}x{j} = {i*j}")
+    
+           
